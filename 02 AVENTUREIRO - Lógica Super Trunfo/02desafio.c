43b6abcecb9c91868aaa75e05dd5e337b59d42ec
anapaulaVS <valezeanapaula123@gmail.com>
2025-03-26 19:56:31 +0000

    Declarando as variáveis para o armazenamento

---
+++ 02 AVENTUREIRO - Lógica Super Trunfo/02desafio.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    printf("DESAFIO LÓGICA SUPER TRUNFO - AVENTUREIRO! \n");
+
+    // Declaração das variáveis para armazenar os dados das Cartas do Jogo
+
+    char estado1, estado2;
+    char codigoCidade1[5], codigoCidade2[5];
+    char nomeCidade1[50], nomeCidade2[50];
+    unsigned long int populacaoCidade1, populacaoCidade2;
+    float areaCidade1, areaCidade2;
+    float pibCidade1, pibCidade2;
+    int pontosTuristicosCidade1, pontosTuristicosCidade2;
+    float densidadePopulacionalCidade1, densidadePopulacionalCidade2;
+
+
+
+
+
+
+    return 0;
+
+}
