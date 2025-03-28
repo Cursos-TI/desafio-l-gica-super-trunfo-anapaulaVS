b1fd219b47d0e13c5f792346699a874d2a244e06
anapaulaVS <valezeanapaula123@gmail.com>
2025-03-28 02:26:19 +0000

    Declarando as variáveis

--- 03 MESTRE - Lógica Super Trunfo/03desafio.c
+++ 03 MESTRE - Lógica Super Trunfo/03desafio.c
@@ -3,6 +3,17 @@
 int main() {
     printf("DESAFIO LÓGICA SUPER TRUNFO - MESTRE!\n");
 
+    // Declaração das variáveis para armazenar os dados das Cartas do Jogo
+
+    char pais1, pais2;
+    char codigoCidade1[5], codigoCidade2[5];
+    char nomeCidade1[50], nomeCidade2[50];
+    unsigned long int populacaoCidade1, populacaoCidade2;
+    float areaCidade1, areaCidade2;
+    float pibCidade1, pibCidade2;
+    int pontosTuristicosCidade1, pontosTuristicosCidade2;
+    float densidadePopulacionalCidade1, densidadePopulacionalCidade2;
+
 
     return 0;
 
