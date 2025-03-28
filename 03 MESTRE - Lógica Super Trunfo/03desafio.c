85ac8d07219c88f085d8db04a7b9d102b04a2048
anapaulaVS <valezeanapaula123@gmail.com>
2025-03-28 22:07:00 +0000

    Cálculo da densidade populacional

--- 03 MESTRE - Lógica Super Trunfo/03desafio.c
+++ 03 MESTRE - Lógica Super Trunfo/03desafio.c
@@ -48,6 +48,11 @@ int main() {
     printf("Digite o número de pontos turísticos da Cidade da Carta 2: ");
     scanf(" %d", &pontosTuristicosCidade2);
 
+    // Cálculo da densidade populacional
+
+    densidadePopulacionalCidade1 = populacaoCidade1 / areaCidade1;
+    densidadePopulacionalCidade2 = populacaoCidade2 / areaCidade2;
+
     
 
 
